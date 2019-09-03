c914ddbcbafd60d5a31a4f3e17f259f64e3221c1
pcaserta <patrick.caserta@live.longwood.edu>
2019-09-03 15:27:17 -0400

    updated some headers and format

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,4 +1,3 @@
-
 // firebase config//
 
 var config = {
@@ -11,109 +10,105 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database()
+var database = firebase.database();
 
 //gloabl variable for deleting trains
-var traincount = 0
-
+var traincount = 0;
 
 //button for adding employees
-$("#add-train-btn").on("click", function (event) {
+$("#add-train-btn").on("click", function(event) {
   event.preventDefault();
 
-  traincount++
+  traincount++;
   //grabbing user inputs
 
-  var TrainName = $("#train-name-input").val().toUpperCase().trim();
-  var Destination = $("#destination-input").val().toUpperCase().trim();
-  var FirstTrain = $("#First-train-input").val().toUpperCase().trim();
-  var Frequency = $("#frequency-input").val().toUpperCase().trim();
+  var TrainName = $("#train-name-input")
+    .val()
+    .toUpperCase()
+    .trim();
+  var Destination = $("#destination-input")
+    .val()
+    .toUpperCase()
+    .trim();
+  var FirstTrain = $("#First-train-input")
+    .val()
+    .toUpperCase()
+    .trim();
+  var Frequency = $("#frequency-input")
+    .val()
+    .toUpperCase()
+    .trim();
 
   //creates local temp object for holding train info
   var newTrain = {
-
     name: TrainName,
     destination: Destination,
     First: FirstTrain,
     rate: Frequency
-  }
+  };
   //uploads train data to database
-  database.ref().push(newTrain)
+  database.ref().push(newTrain);
 
   console.log(newTrain.name);
-
 
   //clear all text boxes
   $("#train-name-input").val("");
   $("#destination-input").val("");
   $("#First-train-input").val("");
   $("#frequency-input").val("");
-
 });
 
 //create firebase event for adding trains to database and throw them in a row on the html//
 
-database.ref().on("child_added", function (childSnapshot) {
-  
-  console.log(childSnapshot.val())
-  
-  
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
 
-  var TrainName = childSnapshot.val().name
-  var Destination = childSnapshot.val().destination
-  var FirstTrain = childSnapshot.val().First
-  var Frequency = childSnapshot.val().rate
-  console.log(Frequency)
+  var TrainName = childSnapshot.val().name;
+  var Destination = childSnapshot.val().destination;
+  var FirstTrain = childSnapshot.val().First;
+  var Frequency = childSnapshot.val().rate;
+  console.log(Frequency);
 
-
-
-  console.log(traincount)
+  console.log(traincount);
   // First train of the day is at 3:30 AM
-  var firstTime = moment(FirstTrain, 'HH:mm')
-  console.log(firstTime)
+  var firstTime = moment(FirstTrain, "HH:mm");
+  console.log(firstTime);
 
   //get current time
 
-  var currentTime = moment()
-  console.log(currentTime)
+  var currentTime = moment();
+  console.log(currentTime);
   //create variable to store the next available train//
 
-
-
-  console.log(traincount)
+  console.log(traincount);
 
   var nextTrain;
   if (firstTime > currentTime) {
     nextTrain = firstTime;
-
   } else {
     // Otherwise, get minutes past first time
-    var minutesPast = currentTime.diff(firstTime, 'minutes');
+    var minutesPast = currentTime.diff(firstTime, "minutes");
     // Find the result of minutesPast % frequency
     var remainder = minutesPast % Frequency;
     // Subtract the remainder from the frequency
     var minutesTilNextTrain = Frequency - remainder;
     // Set nextTrain to the currentTime + `minutesTilNextTrain` minutes
-    nextTrain = currentTime.add(minutesTilNextTrain, 'minutes');
-
+    nextTrain = currentTime.add(minutesTilNextTrain, "minutes");
   }
 
   // Print and format the new train time
-  var nextArrival = nextTrain.format('hh:mm A')
-
+  var nextArrival = nextTrain.format("hh:mm A");
 
   //create button to remove train
 
-  var newRow = $("<tr>")
-  newRow.attr("id", "train-" + traincount)
-
+  var newRow = $("<tr>");
+  newRow.attr("id", "train-" + traincount);
 
   var deleteTrain = $("<button>");
 
   deleteTrain.attr("data-train", traincount);
   deleteTrain.addClass("checkbox");
   deleteTrain.text("X");
-  
 
   newRow.append(
     $("<td>").text(TrainName),
@@ -126,20 +121,13 @@ database.ref().on("child_added", function (childSnapshot) {
 
   $("#customer-table > tbody").append(newRow);
 
-  traincount++
-  
-
+  traincount++;
 });
 
 $(document.body).on("click", ".checkbox", function() {
-
   // Get the number of the button from its data attribute and hold in a variable called  toDoNumber.
   var toDoNumber = $(this).attr("data-train");
 
   // Select and Remove the specific <p> element that previously held the to do item number.
   $("#train-" + toDoNumber).remove();
-  
-  
 });
-
-
